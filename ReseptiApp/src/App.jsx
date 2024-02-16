a033999e5d3235ea899e309181a24eb3de189e3f
Mauno Rytkönen <Mauno.Rytkonen@edu.savonia.fi>
2024-02-16 15:42:39 +0200

    viesti

--- ReseptiApp/src/App.jsx
+++ ReseptiApp/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>12394023904589023929043829042390489042890238903284092384023948902482390349</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
