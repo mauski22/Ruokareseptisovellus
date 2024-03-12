7cb7f0849155532b63b452059f81c14646761927
Toni Kuura <Toni.Kuura@edu.savonia.fi>
2024-03-12 12:14:57 +0200

    ei toimi kortti

--- ReseptiApp/src/components/RecipeDisplay.jsx
+++ ReseptiApp/src/components/RecipeDisplay.jsx
@@ -1,42 +1,45 @@
 import React, { useState, useEffect } from 'react';
-import RecipeCard from './RecipeCard'; // Adjust the path as needed
+import RecipeCard from './RecipeCard';
 import { useAuth } from './AuthContext';
 
-const RecipeDisplay = ({ recipeId }) => {
-  const [recipe, setRecipe] = useState([]);
+const RecipeDisplay = () => {
+  const [recipes, setRecipes] = useState([]);
   const { user } = useAuth();
 
   useEffect(() => {
-    const fetchRecipe = async () => {
+    const fetchRecipes = async () => {
       try {
+        // Ensure the URL is correct and you're correctly passing the user ID
         const response = await fetch(`http://localhost:8081/reseptienIDEIDENHAKU/${user.user_id}`);
         if (!response.ok) throw new Error('Network response was not ok');
         const data = await response.json();
-        console.log(data)
-        setRecipe(data[0]); // Assuming the API returns an array with one object
+        setRecipes(data); // Assuming 'data' is an array of recipe objects
       } catch (error) {
         console.error("Error fetching recipe data:", error);
       }
     };
 
-    fetchRecipe();
-  }, [recipeId]); // Re-fetch when recipeId changes
+    if (user && user.user_id) {
+      fetchRecipes();
+    }
+  }, [user]); // Re-fetch when the user object changes
 
-  // This part remains the same. It ensures that the fetched recipe data
-  // is passed correctly to the RecipeCard component.
   return (
     <div>
-      {recipe ? (
-        <RecipeCard
-          title={recipe.title}
-          time={`${recipe.created_at}`} // Adjust according to your database fields
-          ingredients={recipe.ingredients}
-          instructions={recipe.instructions}
-          tags={recipe.tags} 
-          imageUrl={recipe.image_url} // Make sure to adjust according to your database field
-        />
+      {recipes.length > 0 ? (
+        recipes.map(recipe => (
+          <RecipeCard
+            key={recipe.recipe_id} // Ensure 'recipe_id' is the correct identifier
+            title={recipe.title}
+            time={recipe.time} // Adjust based on your data fields
+            ingredients={recipe.ingredients} // Might need processing if not a string
+            instructions={recipe.instructions}
+            tags={recipe.tags} // Might need processing if not a string
+            imageUrl={recipe.image_url} // Ensure this is correct based on your data
+          />
+        ))
       ) : (
-        <p>Loading recipe...</p>
+        <p>No recipes found for this user.</p>
       )}
     </div>
   );
