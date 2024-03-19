501e7e15628ef71cc5e108f5c3b7af69a9f46b83
Mauno Rytkönen <Mauno.Rytkonen@edu.savonia.fi>
2024-03-19 19:07:13 +0200

    ulkoassuu selkeeyttää

--- ReseptiApp/src/components/AllRecipeDisplay.jsx
+++ ReseptiApp/src/components/AllRecipeDisplay.jsx
@@ -1,12 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from './AuthContext';
-import { Card, Tab, Tabs, Modal } from 'react-bootstrap';
+import { Card, Tab, Tabs, Container, Row, Col, CardGroup, Button } from 'react-bootstrap';
 
 export const AllRecipeDisplay = () => {
   // Oletetaan, että useAuth on konteksti, joka tarjoaa kirjautuneen käyttäjän tiedot
   const { user } = useAuth();
   const [recipes, setRecipes] = useState([]);
+  const [votes, setVotes] = useState({});
+  const [favorites, setFavorites] = useState([]);
 
+
+  const handleVote = (index, type) => {
+    const newVotes = { ...votes };
+    const voteKey = `${index}_${type}`; // Unique key for each recipe and vote type
+    newVotes[voteKey] = (newVotes[voteKey] || 0) + 1;
+    setVotes(newVotes);
+  };
+  const addToFavorites = (recipe) => {
+    alert(`${recipe.title} has been added to favorite recipes!`);
+    // Here you can also update the state if you want to track favorites within the app
+    // For example, to prevent adding the same recipe multiple times, check if it's already a favorite
+    if (!favorites.includes(recipe)) {
+      setFavorites([...favorites, recipe]);
+    }
+  };
   useEffect(() => {
     const fetchRecipes = async () => {
       try {
@@ -24,29 +41,76 @@ export const AllRecipeDisplay = () => {
   }, []);
 
   return (
-    <div className="container" style={{ maxHeight: '1200px', overflowY: 'auto' }}>
+    <div className="container" style={{ maxHeight: '100vh', overflowY: 'auto' }}>
       <h2>Kaikki Reseptit</h2>
-      <p> {"(Jäsenreseptit on merkattu keltaisella)"}</p>
-      <div className="row" style={{ display: 'flex', flexWrap: 'wrap', margin: '1rem' }}>
+      <div className="row" style={{ display: 'flex', flexWrap: 'wrap', margin: '1px' }}>
 
       {recipes.map((recipe, index) => (
-  <Card key={index} className={`recipe-card ${recipe.visibility === 1 ? '' : 'member-only'}`}>
-    <Tabs defaultActiveKey={`tab${index}First`} id={`uncontrolled-tab-example-${index}`}>
-      <Tab eventKey={`tab${index}First`} title="Reseptin etusivu">
-        <h5 className="card-title">{recipe.title}</h5>
-        <p>Julkaisija: {recipe.author_id}</p> {/* Oletetaan että reseptissä on 'author' kenttä */}
-        <p>Resepti luotu: {recipe.created_at}</p>
-        <p>Näkyvyys: {recipe.visibility === 1 ? 'Julkinen' : 'Vain jäsenille'}</p>
-        {recipe.photos && <img src={recipe.photos} alt="Recipe" style={{ width: '50%', height: 'auto' }}/>}
-      </Tab>
-      <Tab eventKey={`tab${index}Reseptin Ainesosat`} title="Reseptin Ainesosat">
-        <p>Ainesosat: {recipe.ingredients}</p>
-      </Tab>
-      <Tab eventKey={`tab${index}Valmistusohje`} title="Valmistusohje">
-        <p className="card-text">{recipe.description}</p>
-      </Tab>
-    </Tabs>
-  </Card>
+          <Card key={index} className={`recipe-card ${recipe.visibility === 1 ? '' : 'member-only'}`} style={{ width: '100vh%', height: '50%' }}>
+          <Row noGutters>
+            <Col md={8}>
+              <Tabs defaultActiveKey={`tab${index}First`} id={`uncontrolled-tab-example-${index}`}>
+               <Tab eventKey={`tab${index}First`} title="Reseptin etusivu">
+                  <h5 className="card-title">{recipe.title}</h5>
+                  <p>Julkaisija: {recipe.author_id}</p>
+                  <p>Resepti luotu: {recipe.created_at}</p>
+                  <p>Näkyvyys: {recipe.visibility === 1 ? 'Julkinen' : 'Vain jäsenille'}</p>
+                  <Button onClick={() => handleVote(index, 'up')} 
+                  style={{ 
+                    marginRight: '10px', 
+                    backgroundColor: 'white', 
+                    borderColor: 'green', 
+                    color: 'black' }} 
+                  size="sm">
+                    👍
+                  </Button>
+                  <>{votes[`${index}_up`] || 0}</>
+                  <Button 
+                    onClick={() => handleVote(index, 'down')} 
+                    style={{ 
+                      marginRight: '10px', 
+                      marginLeft: '20px', 
+                      backgroundColor: 'white', 
+                      borderColor: 'red', 
+                      color: 'black' }} 
+                    size="sm">
+                    👎
+                  </Button>
+                  {votes[`${index}_down`] || 0}
+                  <Button 
+                    onClick={() => addToFavorites(recipe)} 
+                    style={{ 
+                      marginRight: '10px', 
+                      marginLeft: '20px', 
+                      backgroundColor: 'white', 
+                      borderColor: 'black', 
+                      color: 'black' }} 
+                    size="sm">
+                    ⭐
+                  </Button>
+               </Tab>
+               <Tab eventKey={`tab${index}Reseptin Ainesosat`} title="Reseptin Ainesosat">
+                  <ul>
+                    {recipe.ingredients.split(',').map((ingredient, index) => (
+                      <li key={index}>{ingredient.trim()}</li>
+                    ))}
+                  </ul>
+               </Tab>
+               <Tab eventKey={`tab${index}Valmistusohje`} title="Valmistusohje">
+                  <p className="card-text">{recipe.description}</p>
+               </Tab>
+              </Tabs>
+            </Col>
+            <Col md={4} className="image-container">
+              <img 
+               variant="bottom"
+               src={`http://localhost:8081/images/${recipe.photos}`}
+               alt="Recipe"
+               className="custom-image"
+              />
+            </Col>
+          </Row>
+        </Card>
 ))}
     </div>
     </div>
--- ReseptiApp/src/components/PublicRecipeDisplay.jsx
+++ ReseptiApp/src/components/PublicRecipeDisplay.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from './AuthContext';
-import { Card, Tab, Tabs, Modal } from 'react-bootstrap';
+import { Card, Tab, Tabs, Modal, Col, Row } from 'react-bootstrap';
 
 export const PublicRecipeDisplay = () => {
   // Oletetaan, että useAuth on konteksti, joka tarjoaa kirjautuneen käyttäjän tiedot
@@ -24,28 +24,43 @@ export const PublicRecipeDisplay = () => {
   }, []);
 
   return (
-<div className="container" style={{ maxHeight: '1200px', overflowY: 'auto' }}>
-      <p>Liity jäseneksi nähdäksesi kaikki reseptit</p>
-      <div className="row" style={{ display: 'flex', flexWrap: 'wrap', margin: '1rem' }}>
+    <div className="container" style={{ maxHeight: '100vh', overflowY: 'auto' }}>
+      <h2>Kaikki Reseptit</h2>
+      <div className="row" style={{ display: 'flex', flexWrap: 'wrap', margin: '1px' }}>
 
       {recipes.map((recipe, index) => (
-  <Card key={index} className="recipe-card">
-    <Tabs defaultActiveKey={`tab${index}First`} id={`uncontrolled-tab-example-${index}`}>
-      <Tab eventKey={`tab${index}First`} title="Reseptin etusivu">
-        <h5 className="card-title">{recipe.title}</h5>
-        <p>Julkaisija: {recipe.author_id}</p> {/* Oletetaan että reseptissä on 'author' kenttä */}
-        <p>Resepti luotu: {recipe.created_at}</p>
-        <p>Näkyvyys: {recipe.visibility === 1 ? 'Julkinen' : 'Vain jäsenille'}</p>
-        {recipe.photos && <img src={recipe.photos} alt="Recipe" style={{ width: '50%', height: 'auto' }}/>}
-      </Tab>
-      <Tab eventKey={`tab${index}Reseptin Ainesosat`} title="Reseptin Ainesosat">
-        <p>Ainesosat: {recipe.ingredients}</p>
-      </Tab>
-      <Tab eventKey={`tab${index}Valmistusohje`} title="Valmistusohje">
-        <p className="card-text">{recipe.description}</p>
-      </Tab>
-    </Tabs>
-  </Card>
+          <Card key={index} className={`recipe-card ${recipe.visibility === 1 ? '' : 'member-only'}`} style={{ width: '100vh%', height: '50%' }}>
+          <Row noGutters>
+            <Col md={8}>
+              <Tabs defaultActiveKey={`tab${index}First`} id={`uncontrolled-tab-example-${index}`}>
+               <Tab eventKey={`tab${index}First`} title="Reseptin etusivu">
+                  <h5 className="card-title">{recipe.title}</h5>
+                  <p>Julkaisija: {recipe.author_id}</p>
+                  <p>Resepti luotu: {recipe.created_at}</p>
+                  <p>Näkyvyys: {recipe.visibility === 1 ? 'Julkinen' : 'Vain jäsenille'}</p>
+               </Tab>
+               <Tab eventKey={`tab${index}Reseptin Ainesosat`} title="Reseptin Ainesosat">
+                  <ul>
+                    {recipe.ingredients.split(',').map((ingredient, index) => (
+                      <li key={index}>{ingredient.trim()}</li>
+                    ))}
+                  </ul>
+               </Tab>
+               <Tab eventKey={`tab${index}Valmistusohje`} title="Valmistusohje">
+                  <p className="card-text">{recipe.description}</p>
+               </Tab>
+              </Tabs>
+            </Col>
+            <Col md={4} className="image-container">
+              <img 
+               variant="bottom"
+               src={`http://localhost:8081/images/${recipe.photos}`}
+               alt="Recipe"
+               className="custom-image"
+              />
+            </Col>
+          </Row>
+        </Card>
 ))}
     </div>
     </div>
