0f969c1f0c61f17f9a738118379abc0a47c2e837
JuhoRissanen5 <juhorissanen5@gmail.com>
2024-03-20 20:44:32 +0200

    Lempireseptienhaku muutoksia

--- ReseptiApp/src/components/FavoriteRecipes.jsx
+++ ReseptiApp/src/components/FavoriteRecipes.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from './AuthContext';
-import { Card, Tab, Tabs, Col, Row } from 'react-bootstrap';
+import { Card, Tabs, Tab } from 'react-bootstrap'; // Assuming you're using react-bootstrap
 
-const FavoriteRecipes = () => {
+const FavoriteRecipes = ({favorites}) => {
   const [favoriteRecipes, setFavoriteRecipes] = useState([]);
   const { user } = useAuth();
 
@@ -12,14 +12,14 @@ const FavoriteRecipes = () => {
         const response = await fetch(`http://localhost:8081/getFavoriteRecipes/${user.user_id}`, {
           method: 'GET',
           headers: {
-            
+            'Authorization': `Bearer ${user}`,
+            'Content-Type': 'application/json',
           },
         });
 
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
-
         const recipes = await response.json();
         setFavoriteRecipes(recipes);
       } catch (error) {
@@ -28,37 +28,33 @@ const FavoriteRecipes = () => {
     };
 
     fetchFavoriteRecipes();
-  }, [user.user_id, user.token]); // Lisätty riippuvuus käyttäjän ID:hen ja tokeniin
+  }, []);
 
   return (
     <div className="container" style={{ maxHeight: '100vh', overflowY: 'auto' }}>
-      <h2>Suosikki reseptini</h2>
-      <div className="row" style={{ display: 'flex', flexWrap: 'wrap', margin: '1px' }}>
-
-      {favoriteRecipes.map((recipe, index) => (
-  <Card key={index} className="recipe-card" style={{ width: '100vh%', height: '50%' }}>
-    <Row noGutters>
-      <Col md={8}>
-        <Tabs defaultActiveKey={`tab${index}First`} id={`uncontrolled-tab-example-${index}`}>
-          <Tab eventKey={`tab${index}First`} title="Reseptin etusivu">
-            <h5 className="card-title">{recipe.title}</h5>
-            <p>Julkaisija: {recipe.author_id}</p>
-            <p>Resepti luotu: {recipe.created_at}</p>
-            <p>{recipe.description}</p> {/* Oletetaan, että kuvaus on tässä kentässä */}
-          </Tab>
-          {/* Lisää muita välilehtiä tarpeen mukaan */}
-        </Tabs>
-      </Col>
-      <Col md={4} className="image-container">
-        <img 
-          src={`http://localhost:8081/images/${recipe.photos}`}
-          alt="Reseptikuva"
-          className="custom-image"
-        />
-      </Col>
-    </Row>
-  </Card>
-))}
+      <div className="row" style={{ display: 'flex', flexWrap: 'wrap', margin: '1rem' }}>
+        {favoriteRecipes.map((recipe, index) => (
+          <div className="col-lg-4 col-md-6 col-sm-12" key={index} style={{ marginBottom: '1rem' }}>
+            <Card className="recipe-card">
+              <Tabs defaultActiveKey={`tab${index}First`} id={`uncontrolled-tab-example-${index}`}>
+                <Tab eventKey={`tab${index}First`} title="Reseptin etusivu">
+                  <h5 className="card-title">{recipe.title}</h5>
+                  <p>Author: {user.userName}</p>
+                  <p>Created at: {recipe.created_at}</p>
+                  <p>Visiblity: {recipe.visibility === 1 ? 'Näkyy muille' : 'Ei näy muille'}</p>
+                  <img src={`http://localhost:8081/images/${recipe.photos}`} alt="Recipe" style={{ width: '50%', height: 'auto' }}/>
+                  {/* You can add more functionalities here */}
+                </Tab>
+                <Tab eventKey={`tab${index}Reseptin Ainesosat`} title="Reseptin Ainesosat">
+                   {recipe.ingredients}
+                </Tab>
+                <Tab eventKey={`tab${index}Valmistusohje`} title="Valmistusohje">
+                  <p className="card-text">{recipe.description}</p>
+                </Tab>
+              </Tabs>
+            </Card>
+          </div>
+        ))}
       </div>
     </div>
   );
--- backend/server.js
+++ backend/server.js
@@ -313,14 +313,19 @@ app.post('/favoritesLisays', (req, res) => {
 })
 app.get('/getFavoriteRecipes/:id', (req, res) => {
     try {
-        const sql = "SELECT favorite_id, recipe_id FROM favorites WHERE user_id = ?";
+        const sql = "SELECT r.recipe_id, r.title, r.author_id, r.description, r.visibility, DATE(r.created_at) AS created_at, DATE(r.updated_at) AS updated_at, GROUP_CONCAT(DISTINCT CONCAT(i.name, ' (' , i.quantity, ')')) AS ingredients, GROUP_CONCAT(DISTINCT p.image SEPARATOR ', ') AS photos FROM recipes r LEFT JOIN ingredients i ON r.recipe_id = i.recipe_id LEFT JOIN photos p ON r.recipe_id = p.recipe_id JOIN favorites f ON r.recipe_id = f.recipe_id WHERE f.user_id = ? GROUP BY r.recipe_id;";
         const userid = req.params.id
         db.query (sql, [userid], (err, data) => {
             if(err){
                 console.error("Error fetching favorites:" + err)
-                return res.status(500).json("Error fetching favorites:" + err)
             }
+            else {
+                data.forEach(row => {
+                    row.created_at = row.created_at.toISOString().split('T')[0];
+                    row.updated_at = row.updated_at.toISOString().split('T')[0];
+                });
             return res.status(200).json(data)
+            }
         })
     }
     catch (error) {
