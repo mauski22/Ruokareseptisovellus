346b703ecd13600dd33a59a0391a67e17ea5d8ff
Aleksi Hyvärinen <aleksi.hyvarinen@edu.savonia.fi>
2024-03-22 15:25:04 +0200

    kuva piiloon tab vaihdos

--- ReseptiApp/src/components/FavoriteRecipes.jsx
+++ ReseptiApp/src/components/FavoriteRecipes.jsx
@@ -51,43 +51,40 @@ const FavoriteRecipes = () => {
 
 
   return (
-    <div className="container" style={{ maxHeight: '100vh', overflowY: 'auto' }}>
-      <h2>Suosikki Reseptit</h2>
-      <div className="row" style={{ display: 'flex', flexWrap: 'wrap', margin: '1px' }}>
-        {favoriteRecipes.map((recipe, index) => (
-          <Card key={index} className="recipe-card" style={{ width: '100%', margin: '10px' }}>
-            <Row noGutters>
-              <Col md={8}>
-                <Tabs defaultActiveKey={`tab${index}First`} id={`uncontrolled-tab-example-${index}`}>
-                  <Tab eventKey={`tab${index}First`} title="Reseptin etusivu">
-                    <h5 className="card-title">{recipe.title}</h5>
-                    <p>Julkaisija: {recipe.author_nickname}</p> {/* Oletetaan että suosikkidatassa on julkaisijan tieto */}
-                    <p>Resepti luotu: {recipe.created_at}</p>
-                    <p>Näkyvyys: {recipe.visibility === 1 ? 'Julkinen' : 'Vain jäsenille'}</p>
-                    {/* Esimerkkiä ei voi suoraan käyttää, mutta voit lisätä toiminnallisuuksia tarpeen mukaan */}
-                  </Tab>
-                  <Tab eventKey={`tab${index}Reseptin Ainesosat`} title="Reseptin Ainesosat">
-                    <p>{recipe.ingredients}</p>
-                  </Tab>
-                  <Tab eventKey={`tab${index}Valmistusohje`} title="Valmistusohje">
-                    <p className="card-text">{recipe.description}</p>
-                  </Tab>
-            
-                </Tabs>
-              </Col>
-              <Col md={4} className="image-container">
+<div className="container" style={{ maxHeight: '100vh', overflowY: 'auto' }}>
+  <h2>Suosikki Reseptit</h2>
+  <div className="row" style={{ display: 'flex', flexWrap: 'wrap', margin: '1px' }}>
+    {favoriteRecipes.map((recipe, index) => (
+      <Card key={index} className="recipe-card" style={{ width: '100%', margin: '10px' }}>
+        <Row noGutters>
+          <Col md={8}>
+            <Tabs defaultActiveKey={`tab${index}First`} id={`uncontrolled-tab-example-${index}`}>
+              <Tab eventKey={`tab${index}First`} title="Reseptin etusivu">
                 <img
                   src={`http://localhost:8081/images/${recipe.photos}`}
                   alt="Recipe"
                   className="custom-image"
-                  style={{ width: '100%', height: 'auto' }} />
-              </Col>
-            </Row>
-            
-          </Card>
-        ))}
-      </div>
-    </div>
+                  style={{ width: '50%', height: 'auto' }} />
+                <h5 className="card-title">{recipe.title}</h5>
+                <p>Julkaisija: {recipe.author_nickname}</p>
+                <p>Resepti luotu: {recipe.created_at}</p>
+                <p>Näkyvyys: {recipe.visibility === 1 ? 'Julkinen' : 'Vain jäsenille'}</p>
+              </Tab>
+              <Tab eventKey={`tab${index}Reseptin Ainesosat`} title="Reseptin Ainesosat">
+                <p>{recipe.ingredients}</p>
+              </Tab>
+              <Tab eventKey={`tab${index}Valmistusohje`} title="Valmistusohje">
+                <p className="card-text">{recipe.description}</p>
+              </Tab>
+            </Tabs>
+          </Col>
+          {/* Ota pois Col-md={4} osio, jossa alkuperäinen kuva sijaitsi */}
+        </Row>
+      </Card>
+    ))}
+  </div>
+</div>
+
   );
 };
 
