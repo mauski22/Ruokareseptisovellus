88e2a6b202e8bb7a53b7d1c75162c39bfde64908
Mauno Rytkönen <Mauno.Rytkonen@edu.savonia.fi>
2024-04-09 14:03:59 +0300

    Yritysteksti navbar huge cook

--- ReseptiApp/src/components/NavigationBar.jsx
+++ ReseptiApp/src/components/NavigationBar.jsx
@@ -100,7 +100,7 @@ const NavigationBar = ({ onLoginClicked, onSignUpClicked }) => {
             {user ? (
               <>
                 <Navbar.Text className="me-2">
-                  Kirjauduttu sisään: {user.userName} {isSuperAdmin ? '[ADMIN]' : '(Jäsen)'}
+                  Kirjauduttu sisään: {user.userName} {isSuperAdmin ? 'YRITYS' : 'Jäsen'}
                 </Navbar.Text>
                 <Button onClick={handleShowConfirmModal} variant="outline-danger">Kirjaudu ulos</Button>
               </>
